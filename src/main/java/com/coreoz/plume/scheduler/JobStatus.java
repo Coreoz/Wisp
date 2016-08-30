5c0e5b28445cd5f542ec3dcc9542bee0c2db7bbf
Aurélien Manteaux <amanteaux@cardiweb.com>
2016-08-30 22:40:13 +0200

    Javadoc

--- src/main/java/com/coreoz/plume/scheduler/JobStatus.java
+++ src/main/java/com/coreoz/plume/scheduler/JobStatus.java
@@ -2,9 +2,21 @@ package com.coreoz.plume.scheduler;
 
 public enum JobStatus {
 
+	/**
+	 * will not be run ever again
+	 */
 	DONE,
+	/**
+	 * will be executed again, but not right now
+	 */
 	SCHEDULED,
+	/**
+	 * a scheduled job that will probably run sooner than another scheduled job :)
+	 */
 	READY,
+	/**
+	 * a job currently running
+	 */
 	RUNNING,
 
 }
