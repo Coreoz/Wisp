1db837c2f676ec7b2f4df0060ea74fe2c5a9d5f9
Aurélien Manteaux <amanteaux@gmail.com>
2016-10-02 12:18:40 +0200

    Dagger Plume module renamed

--- src/main/java/com/coreoz/plume/scheduler/dagger/DaggerSchedulerModule.java
+++ src/main/java/com/coreoz/plume/scheduler/dagger/DaggerSchedulerModule.java
@@ -2,14 +2,14 @@ package com.coreoz.plume.scheduler.dagger;
 
 import javax.inject.Singleton;
 
-import com.coreoz.plume.dagger.DaggerCoreModule;
+import com.coreoz.plume.dagger.DaggerServicesModule;
 import com.coreoz.plume.scheduler.Scheduler;
 import com.coreoz.plume.scheduler.plume.SchedulerProvider;
 
 import dagger.Module;
 import dagger.Provides;
 
-@Module(includes = DaggerCoreModule.class)
+@Module(includes = DaggerServicesModule.class)
 public class DaggerSchedulerModule {
 
 	@Provides
