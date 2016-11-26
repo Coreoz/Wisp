bfc6fd51acb75970eb3149e724ec47cd4feea7d0
Aurélien Manteaux <amanteaux@gmail.com>
2016-11-26 16:37:52 +0100

    Ensure CRON based jobs are not executed twice if job execution last 0ms

--- src/test/java/com/coreoz/wisp/schedule/cron/CronScheduleTest.java
+++ src/test/java/com/coreoz/wisp/schedule/cron/CronScheduleTest.java
@@ -3,6 +3,9 @@ package com.coreoz.wisp.schedule.cron;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import org.junit.Test;
 
@@ -22,6 +25,22 @@ public class CronScheduleTest {
 
 		assertThat(everyMinuteScheduler.nextExecutionInMillis(0, 0, null))
 		.isEqualTo(Duration.ofMinutes(1).toMillis());
+	}
+
+	@Test
+	public void should_not_executed_daily_jobs_twice_a_day() {
+		CronSchedule everyMinuteScheduler = CronSchedule.parseQuartzCron("0 0 12 * * ? *");
+
+		ZonedDateTime augustMidday = LocalDate
+			.of(2016, 8, 31)
+			.atTime(12, 0)
+			.atZone(ZoneId.systemDefault());
+		long midday = augustMidday.toEpochSecond() * 1000;
+
+		assertThat(everyMinuteScheduler.nextExecutionInMillis(midday-1, 0, null))
+		.isEqualTo(midday);
+		assertThat(everyMinuteScheduler.nextExecutionInMillis(midday, 0, null))
+		.isEqualTo(midday + Duration.ofDays(1).toMillis());
 	}
 
 }
