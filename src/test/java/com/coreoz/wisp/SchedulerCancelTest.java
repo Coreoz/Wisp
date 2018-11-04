dec967b0c2df06dd73a3fa7f192e8c1423c2e13a
Aurélien Manteaux <amanteaux@gmail.com>
2018-11-04 22:32:19 +0100

    Tests refactoring to improve readability

---
+++ src/test/java/com/coreoz/wisp/SchedulerCancelTest.java
@@ -0,0 +1,116 @@
+package com.coreoz.wisp;
+
+import static com.coreoz.wisp.Utils.doNothing;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+import java.time.Duration;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Test;
+
+import com.coreoz.wisp.Utils.SingleJob;
+import com.coreoz.wisp.schedule.Schedules;
+
+/**
+ * Tests about {@link Scheduler#cancel(String)} only
+ */
+public class SchedulerCancelTest {
+
+	@Test
+	public void cancel_should_throw_IllegalArgumentException_if_the_job_name_does_not_exist() {
+		Scheduler scheduler = new Scheduler(SchedulerConfig.builder().maxThreads(1).build());
+		try {
+			scheduler.cancel("job that does not exist");
+			fail("Should not accept to cancel a job that does not exist");
+		} catch (IllegalArgumentException e) {
+			// as expected :)
+		}
+		scheduler.gracefullyShutdown();
+	}
+
+	@Test
+	public void cancel_should_returned_a_job_with_the_done_status() throws Exception {
+		Scheduler scheduler = new Scheduler(SchedulerConfig.builder().maxThreads(1).build());
+		scheduler.schedule("doNothing", doNothing(), Schedules.fixedDelaySchedule(Duration.ofMillis(100)));
+		Job job = scheduler.cancel("doNothing").toCompletableFuture().get(1, TimeUnit.SECONDS);
+
+		assertThat(job).isNotNull();
+		assertThat(job.status()).isEqualTo(JobStatus.DONE);
+		assertThat(scheduler.jobStatus().size()).isEqualTo(1);
+		assertThat(job.name()).isEqualTo("doNothing");
+		assertThat(job.runnable()).isSameAs(doNothing());
+
+		scheduler.gracefullyShutdown();
+
+		assertThat(job.executionsCount()).isEqualTo(0);
+	}
+
+	@Test
+	public void cancelled_job_should_be_schedulable_again() throws Exception {
+		Scheduler scheduler = new Scheduler(SchedulerConfig.builder().maxThreads(1).build());
+		scheduler.schedule("doNothing", doNothing(), Schedules.fixedDelaySchedule(Duration.ofMillis(100)));
+		scheduler.cancel("doNothing").toCompletableFuture().get(1, TimeUnit.SECONDS);
+
+		Job job = scheduler.schedule("doNothing", doNothing(), Schedules.fixedDelaySchedule(Duration.ofMillis(100)));
+
+		assertThat(job).isNotNull();
+		assertThat(job.status()).isEqualTo(JobStatus.SCHEDULED);
+		assertThat(job.name()).isEqualTo("doNothing");
+		assertThat(job.runnable()).isSameAs(doNothing());
+
+		scheduler.gracefullyShutdown();
+	}
+
+	@Test
+	public void cancelling_a_job_should_wait_until_it_is_terminated_and_other_jobs_should_continue_running()
+			throws InterruptedException, ExecutionException, TimeoutException {
+		Scheduler scheduler = new Scheduler(SchedulerConfig.builder().maxThreads(1).build());
+
+		SingleJob jobProcess1 = new SingleJob();
+		SingleJob jobProcess2 = new SingleJob() {
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(200);
+					super.run();
+				} catch (InterruptedException e) {
+					throw new RuntimeException("Should not be interrupted", e);
+				}
+			}
+		};
+
+		Job job1 = scheduler.schedule(jobProcess1, Schedules.fixedDelaySchedule(Duration.ofMillis(1)));
+		Job job2 = scheduler.schedule(jobProcess2, Schedules.afterInitialDelay(
+			Schedules.fixedDelaySchedule(Duration.ofMillis(1)),
+			Duration.ofMillis(5)
+		));
+
+		Thread.sleep(10);
+
+		int job1ExecutionsCount = job1.executionsCount();
+		assertThat(job1ExecutionsCount).isGreaterThan(0);
+		assertThat(job2.executionsCount()).isEqualTo(0);
+
+		scheduler.cancel(job2.name()).toCompletableFuture().get(1, TimeUnit.SECONDS);
+
+		Thread.sleep(30);
+		scheduler.gracefullyShutdown();
+
+		assertThat(job2.executionsCount()).isEqualTo(1);
+		assertThat(jobProcess2.countExecuted.get()).isEqualTo(1);
+		// after job 2 is cancelled, job 1 should have been executed at least 5 times
+		assertThat(job1.executionsCount()).isGreaterThan(job1ExecutionsCount + 5);
+	}
+
+	@Test
+	public void cancelling_a_job_should_wait_until_it_is_terminated_and_other_jobs_should_continue_running__races_test()
+			throws Exception {
+		for(int i=0; i<5; i++) {
+			cancelling_a_job_should_wait_until_it_is_terminated_and_other_jobs_should_continue_running();
+		}
+	}
+
+}
---
+++ src/test/java/com/coreoz/wisp/Utils.java
@@ -0,0 +1,40 @@
+package com.coreoz.wisp;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+
+public class Utils {
+
+	public static class SingleJob implements Runnable {
+		AtomicInteger countExecuted = new AtomicInteger(0);
+
+		@Override
+		public void run() {
+			countExecuted.incrementAndGet();
+			synchronized (this) {
+				notifyAll();
+			}
+		}
+	}
+
+	public static void waitOn(Object lockOn, Supplier<Boolean> condition, long maxWait) {
+		long currentTime = System.currentTimeMillis();
+		long waitUntil = currentTime + maxWait;
+		while(!condition.get() && waitUntil > currentTime) {
+			synchronized (lockOn) {
+				try {
+					lockOn.wait(5);
+				} catch (InterruptedException e) {
+				}
+			}
+			currentTime = System.currentTimeMillis();
+		}
+	}
+
+	// a do nothing runnable
+	private static Runnable doNothing = () -> {};
+	public static Runnable doNothing() {
+		return doNothing;
+	}
+
+}
